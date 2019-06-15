8d4d16d45aa6072d87f257d6c660ad81b80f9f74
Sagar Rajput <sagartubearn@gmail.com>
2019-06-15 17:51:38 +0530

    Version 1.0

---
+++ Assignment/Assignment/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        Crudoperation CrudObj = new Crudoperation();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount user)
+        {
+            CrudObj.AddUser(user);
+            ModelState.Clear();
+            Response.Write("Account Succesfully Added");
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+        {
+            if(ModelState.IsValid)
+            {
+                var usr = CrudObj.GetUserInfo.Single(usr => usr.UserName == user.UserName && usr.Password == user.Password);
+                Response.Write("Login Succesful");
+
+            }
+
+            else
+            {
+                ModelState.AddModelError("", "UserName and Password MisMatch");
+            }
+
+            return View();
+                 
+        }
+    }
+}
---
+++ Assignment/Assignment/Models/UserAccount.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    public class UserAccount
+    {
+        [key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="Please Enter FirstName")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage ="Please Enter LastName")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Email-Id")]
+        [EmailAddress(ErrorMessage ="Please Enter a Valid Email-Id")]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage ="Please Enter a Valid UserName")]
+        public string UserName { get; set; }
+
+        [Required (ErrorMessage ="Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Password Not Matching")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class Crudoperation
+    {
+        string str = ConfigurationManager.ConnectionStrings["CrudConnection"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+
+        public IEnumerable<UserAccount> GetUserInfo
+        {
+            get
+            {
+                con = new SqlConnection(str);
+                con.Open();
+                cmd = new SqlCommand();
+                cmd.CommandText = "SSpGetAll";
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+
+                List<UserAccount> user = new List<UserAccount>();
+
+                while (dr.Read())
+                {
+                    UserAccount usr = new UserAccount();
+                    usr.UserName = dr["UserName"].ToString();
+                    usr.Password = dr["Password"].ToString();
+
+                    user.Add(usr);
+                }
+
+                dr.Close();
+                con.Close();
+
+                return user;
+            }
+        }
+        public void AddUser(UserAccount user)
+        {
+            con = new SqlConnection(str);
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandText = "SSpInsertAll";
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", user.LastName);
+            cmd.Parameters.AddWithValue("@EmailId", user.EmailId);
+            cmd.Parameters.AddWithValue("@UserName", user.UserName);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+}
